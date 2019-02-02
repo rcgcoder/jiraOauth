ba6bb9fbeaafa0798dd2c602b2d0d3b1dc9e61f2
rcantabrana <rcantabrana@A0108030.ARAGON>
2019-02-02 21:29:38 +0100

    control binary

--- app/app.js
+++ app/app.js
@@ -389,6 +389,8 @@ app.get('/atlassian/call', function(request, response){
 //		undefined,
 		content_type,
 		function(error, data, resp){
+			var bIsBin=isBinary(null, data);
+			console.log("Is binary:"+bIsBin);
 			console.log(data);
 			//console.log("First byte:"+data.charCodeAt(0));
 			//data = JSON.parse(data);
